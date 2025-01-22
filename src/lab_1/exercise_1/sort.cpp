ddc82ca5dffc62f063b7210ad2e0991179207ea3
Narendra S <narendra24110064@snuchennai.edu.in>
2025-01-22 15:49:50 +0530

    Lab 1 Exercise 1

---
+++ src/lab_1/exercise_1/sort.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+void menu(void);
+void input(int [], int *);
+void bubble_sort(int [], int);
+void insertion_sort(int [], int);
+void select_sort(int [], int);
+
+int main(void) {
+    return 0;   
+}
