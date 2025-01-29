8d7d675caf0089ae0c39521378327781a7b08ba4
Narendra S <narendra24110064@snuchennai.edu.in>
2025-01-29 11:26:19 +0530

    lab 2 source and output screenshots

---
+++ src/lab_2/exercise_1/pointer.cpp
@@ -0,0 +1,166 @@
+/*
+  Write C++ menu driven program to determine if a given number is a pallindrome, an armstrong number or a perfect number,
+  normal variable allocations are not allowed, only heap allocated pointers.
+*/
+
+
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+void is_pallindrome(int*, void*, bool*);
+void is_armstrong(int*, void*, bool*);
+void power(int *, int*, int*);
+void get_order(int*, int*);
+void is_perfect(int*, void*, bool*);
+void menu(void*);
+
+int main(void) {
+    // Chunk of memory for variables that are going to be used in menu function
+    // so as to not re request memory on the heap for each iteration.
+    // freed memory in exit case of menu.
+    void *chunk = malloc(
+        sizeof(char) + sizeof(int) + sizeof(bool) // for menu function itself
+        + sizeof(int) * 2 // For sub functions pallindrome, armstrong and perfect numbers
+        + sizeof(int) * 4 // Armstrong number itself requires 1 more integer variable, power requires 3 more
+        );
+
+    if (chunk == NULL) {
+        cout << "Error allocating memory!";
+        return 1;
+    }
+
+    while (true)
+        menu(chunk);
+}
+
+// Takes in a chunk of heap allocated memory for assigning variables
+// Calls appropriate function according to menu input
+void menu(void *chunk) {
+    char *input = (char *) chunk;
+    int *num_check = (int *) (chunk + sizeof(char));
+    bool *ret = (bool *) (chunk + sizeof(char) + sizeof(int));
+
+    void *unused = chunk + sizeof(char) + sizeof(int) + sizeof(bool);
+
+    cout << "Enter number to check: ";
+    cin >> *num_check;
+
+    if (*num_check < 0) {
+        cout << "Number must be positive!" << endl;
+        return;
+    }
+
+    cout << "a. Palindrome" << endl << "b. Armstrong number" << endl << "c. Perfect number" << endl << "d. Exit" << endl;
+    cout << "Enter check to perform: ";
+    cin >> *input;
+
+    switch (*input) {
+
+    case 'a':
+    case 'A':
+        is_pallindrome(num_check, unused, ret);
+        cout << "The given number " << *num_check << " is" << (*ret ? " " : " not ") << "a pallindrome" << endl;
+        break;
+
+    case 'b':
+    case 'B':
+        is_armstrong(num_check, unused, ret);
+        cout << "The given number " << *num_check << " is" << (*ret ? " " : " not ") << "an armstrong number" << endl;
+        break;
+
+    case 'c':
+    case 'C':
+        is_perfect(num_check, unused,ret);
+        cout << "The given number " << *num_check << " is" << (*ret ? " " : " not ") << "a perfect number" << endl;
+        break;
+
+    case 'd':
+    case 'D':
+        free(chunk);
+        exit(0);
+        break;
+
+    default:
+        cout << "Invalid choice!" << endl;
+    }
+
+    cout << endl << endl;
+}
+
+// Takes in an integer and returns (by value) boolean indicating whether given number is a pallindrome or not
+// Also takes in chunk of heap allocated memory for usage
+void is_pallindrome(int *num, void *chunk, bool* ret) {
+    int *cpy = (int *) chunk;
+    int *rev = cpy + 1;
+
+    *cpy = *num,
+    *rev = 0;
+
+    while (*cpy != 0) {
+        *rev *= 10;
+        *rev += *cpy % 10;
+        *cpy /= 10;
+    }
+    
+    *ret = (*rev == *num);
+}
+
+// Takes in an integer and returns (by value) boolean indicating whether given number is an armstrong number or not
+// Also takes in chunk of heap allocated memory for usage
+void is_armstrong(int *num, void *chunk, bool* ret) {
+    int *cpy = (int *) chunk;
+    int *order = cpy + 1;
+    int *order_cpy = cpy + 2; // variable to pass to power as it modifies value, keep actual order in order and use this whenever power is to be calculated
+    int *sum = cpy + 3;
+    int *pow = cpy + 4;
+    int *digit = cpy + 5;
+
+    *cpy = *num;
+
+    get_order(cpy, order);
+
+    *cpy = *num;
+    *sum = 0;
+
+    while (*cpy != 0) {
+        *order_cpy = *order;
+        *digit = *cpy % 10;
+        power(digit, order_cpy, pow);
+        *sum += *pow;
+        *cpy /= 10;
+    }
+
+    *ret = (*sum == *num);
+}
+
+// Helper function to armstrong for calculating powers
+void power(int *base, int *power, int *ret) {
+    for (*ret = 1;*power > 0; (*power)--)
+        *ret *= *base;
+}
+
+// Helper function to armstrong for calculating order of a given number
+void get_order(int *num, int* ret) {
+    *ret = 0;
+    while (*num != 0) {
+        *num /= 10;
+        *ret += 1;
+    }
+}
+
+// Takes in an integer and returns (by value) boolean indicating whether given number is a perfect number or not
+// Also takes in chunk of heap allocated memory for usage
+void is_perfect(int *num, void *chunk, bool* ret) {
+    int *i = (int *)chunk;
+    int *sum = i + 1;
+
+    for (*i = 1, *sum = 0; *i < *num; (*i)++) {
+        if (!(*num % *i)) {
+            *sum += *i;
+        }
+    }
+
+    *ret = (*sum == *num);
+}
---
+++ src/lab_2/exercise_2/classes.cpp
@@ -0,0 +1,161 @@
+#include <iostream>
+
+using namespace std;
+
+
+class Square {
+    private:
+        unsigned int length;
+    public:
+        Square(unsigned int length): length(length) {}
+        Square(): length(1) {}
+
+        unsigned int get_area();
+};
+
+
+
+class Cube {
+    private:
+        unsigned int length;
+    public:
+        Cube(unsigned int length): length(length) {}
+        Cube(): length(1) {}
+
+        unsigned int get_area();
+};
+
+
+
+class Rectangle {
+    private:
+        unsigned int length, breadth;
+    public:
+        Rectangle(unsigned int length, unsigned int breadth): length(length), breadth(breadth) {}
+        Rectangle(): length(1), breadth(1) {}
+
+        unsigned int get_area();
+};
+
+
+
+class Cuboid {
+    private:
+        unsigned int length, breadth, height;
+    public:
+        Cuboid(unsigned int length, unsigned int breadth, unsigned int height): length(length), breadth(breadth), height(height) {}
+        Cuboid(): length(1), breadth(1), height(1) {}
+
+        unsigned int get_area();
+};
+
+
+
+void menu(void);
+
+
+int main(void) {
+
+    while (true)
+        menu();
+
+    return 0;
+}
+
+
+void menu(void) {
+    char choice;
+
+    cout << "a. Square" << endl
+    << "b. Cube" << endl
+    << "c. Rectangle" << endl
+    << "d. Cuboid" << endl
+    << "e. Exit" << endl;
+
+    cout << "Enter choice: ";
+    cin >> choice;
+
+    int length, breadth, height;
+    Square sqr;
+    Cube cube;
+    Rectangle rect;
+    Cuboid cuboid;
+
+    switch (choice) {
+    case 'a':
+    case 'A':
+        cout << "Enter length: ";
+        cin >> length;
+        sqr = Square(length);
+
+        cout << "Area of given square of side length "
+        << length << " is " << sqr.get_area() << " units" << endl;
+        break;
+
+    case 'b':
+    case 'B':
+        cout << "Enter length: ";
+        cin >> length;
+        cube = Cube(length);
+
+        cout << "Surface area of give cube of side length "
+        << length << " is " << cube.get_area() << " units" << endl;
+        break;
+    
+    case 'c':
+    case 'C':
+        cout << "Enter length: ";
+        cin >> length;
+        cout << "Enter breadth: ";
+        cin >> breadth;
+        rect = Rectangle(length, breadth);
+
+        cout << "Area of given rectangle of side length "
+        << length << " and breadth " << breadth << " is " << rect.get_area() << " units" << endl;
+        break;
+
+    case 'd':
+    case 'D':
+        cout << "Enter length: ";
+        cin >> length;
+        cout << "Enter breadth: ";
+        cin >> breadth;
+        cout << "Enter height: ";
+        cin >> height;
+        cuboid = Cuboid(length, breadth, height);
+
+        cout << "Surface area of given cuboid of side length "
+        << length << ", breadth " << breadth << " and height "<< height 
+        << " is " << cuboid.get_area() << " units" << endl;
+        break;
+
+    case 'e':
+    case 'E':
+        exit(0);
+
+    default:
+        cout << "Invalid choice!" << endl;
+    }
+
+    cout << endl << endl;
+}
+
+
+unsigned int Square::get_area() {
+    return this->length * this->length;
+}
+
+
+unsigned int Cube::get_area() {
+    return 6 * this->length * this->length;
+}
+
+
+unsigned int Rectangle::get_area() {
+    return this->length * this->breadth;
+}
+
+
+unsigned int Cuboid::get_area() {
+    return 2 * (this->length * this->breadth + this->length * this->height + this->breadth * this->height);
+}
